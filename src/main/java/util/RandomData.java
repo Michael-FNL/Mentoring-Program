5c128191a136ac1a44fb415ee41d28e35dcabd44
helen <mdvurechenskyi@gmail.com>
2025-07-15 08:14:37 +0300

    Module #9: CI/CD

--- src/main/java/util/RandomData.java
+++ src/main/java/util/RandomData.java
@@ -8,14 +8,14 @@ public class RandomData {
     public static final int RANDOM_NUMBER_LENGTH_TWO = 2;
     public static final int RANDOM_NUMBER_LENGTH_TEN = 10;
 
-    private static final SecureRandom secureRandom = new SecureRandom();
+    private static final SecureRandom SECURE_RANDOM = new SecureRandom();
     private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     private static final String ALPHANUMERIC = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
     public static String getRandomString() {
         StringBuilder sb = new StringBuilder(RANDOM_STRING_LENGTH);
         for (int i = 0; i < RANDOM_STRING_LENGTH; i++) {
-            int index = secureRandom.nextInt(ALPHABET.length());
+            int index = SECURE_RANDOM.nextInt(ALPHABET.length());
             sb.append(ALPHABET.charAt(index));
         }
         return sb.toString();
@@ -26,10 +26,11 @@ public class RandomData {
     }
 
     public static Integer getRandomInteger() {
-        int length = RANDOM_NUMBER_LENGTH_ONE + secureRandom.nextInt(RANDOM_NUMBER_LENGTH_TWO - RANDOM_NUMBER_LENGTH_ONE + 1);
+        int length = RANDOM_NUMBER_LENGTH_ONE + SECURE_RANDOM
+                .nextInt(RANDOM_NUMBER_LENGTH_TWO - RANDOM_NUMBER_LENGTH_ONE + RANDOM_NUMBER_LENGTH_ONE);
         StringBuilder sb = new StringBuilder(length);
         for (int i = 0; i < length; i++) {
-            int digit = secureRandom.nextInt(10);
+            int digit = SECURE_RANDOM.nextInt(RANDOM_NUMBER_LENGTH_TEN);
             sb.append(digit);
         }
         return Integer.parseInt(sb.toString());
@@ -38,7 +39,7 @@ public class RandomData {
     public static String getRandomAlphaNumeric() {
         StringBuilder sb = new StringBuilder(RANDOM_NUMBER_LENGTH_TEN);
         for (int i = 0; i < RANDOM_NUMBER_LENGTH_TEN; i++) {
-            int index = secureRandom.nextInt(ALPHANUMERIC.length());
+            int index = SECURE_RANDOM.nextInt(ALPHANUMERIC.length());
             sb.append(ALPHANUMERIC.charAt(index));
         }
         return sb.toString();
